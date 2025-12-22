35e978ed19a814fe2b8595d52dcb4705f2851afd
sohan.eluri <elurisohan8@gmail.com>
2025-12-21 19:16:39 -0500

    updating end point

---
+++ Backend/src/main/java/com/tracknote/controller/TaskController.java
@@ -0,0 +1,4 @@
+package com.tracknote.controller;
+
+public class TaskController {
+}
--- Backend/src/main/java/com/tracknote/service/TaskService.java
+++ Backend/src/main/java/com/tracknote/service/TaskService.java
@@ -22,46 +22,7 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
     private final ProjectRepository projectRepository;
-    //create
-
-    public Task createTask(int projectId, TaskDTO dto){
-        Project project=getProject(projectId);
-        Task task= Task.builder().name(dto.getName()).description(dto.getDescription()).status(dto.getStatus()).priority(dto.getPriority()).dueDate(dto.getDueDate()).project(project).build();
-        return taskRepository.save(task);
-    }
-
-    public List<Task> getTasksByProject(int projectId){
-        Project project=getProject(projectId);
-        return taskRepository.findByProjectId(projectId);
-    }
-
-
-    public Task updateTask(int id,TaskDTO updatedTask){
-        Task task=getTask(id);
-        task.setName(updatedTask.getName());
-        task.setDescription(updatedTask.getDescription());
-        task.setStatus(updatedTask.getStatus());
-        task.setPriority(updatedTask.getPriority());
-        task.setDueDate(updatedTask.getDueDate());
-        return taskRepository.save(task);
-    }
-
-    public void deleteTask(int id){
-        taskRepository.deleteById(id);
-    }
-
-    //pagination
-    public Page<Task> getTasks(Status status, Integer projectId, Pageable pageable) {
-        if (status != null && projectId != null) {
-            return taskRepository.findByStatusAndProjectId(status, projectId,pageable);
-        } else if (status != null && projectId == null) {
-            return taskRepository.findByStatus(status,pageable);
-        } else if (status == null && projectId != null) {
-            return taskRepository.findByProjectId(projectId,pageable);
-        } else {
-            return taskRepository.findAll(pageable);
-        }
-    }
+ 
 
     private Task getTask(final int id){
         return taskRepository.findById(id).orElseThrow(()->new TaskNotFoundException(id));
